a70c7f45c01ec065d3457b746002f1e84db1d2df
Antony Caporossi <antony.caporossi@gmail.com>
2024-12-04 13:23:47 +0100

    StramingCommunity update domain

--- StreamingcommunityProvider/src/main/kotlin/com/lagradost/StreamingcommunityProvider.kt
+++ StreamingcommunityProvider/src/main/kotlin/com/lagradost/StreamingcommunityProvider.kt
@@ -19,8 +19,8 @@ import java.net.URLDecoder
 import org.json.JSONObject
 
 class StreamingcommunityProvider : MainAPI() {
-    override var mainUrl = "https://streamingcommunity.computer"
-    private var cdnUrl = "https://cdn.streamingcommunity.computer" // Images
+    override var mainUrl = "https://streamingcommunity.asia"
+    private var cdnUrl = "https://cdn.streamingcommunity.asia" // Images
     private var xInertiaVersion = "759de90f13813ff6f03369b34b51a141"
     override var name = "StreamingCommunity"
     override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
