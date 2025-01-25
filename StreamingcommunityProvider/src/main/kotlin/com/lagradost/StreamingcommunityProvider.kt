f0182aa5f054ef55962d5cf4522b6733c5e774db
Antony Caporossi <antony.caporossi@gmail.com>
2025-01-25 14:17:42 +0100

    Fix: Streamingcommunity domain

--- StreamingcommunityProvider/src/main/kotlin/com/lagradost/StreamingcommunityProvider.kt
+++ StreamingcommunityProvider/src/main/kotlin/com/lagradost/StreamingcommunityProvider.kt
@@ -20,8 +20,8 @@ import org.json.JSONObject
 import com.lagradost.cloudstream3.network.CloudflareKiller
 
 class StreamingcommunityProvider : MainAPI() {
-    override var mainUrl = "https://streamingcommunity.ooo"
-    private var cdnUrl = "https://cdn.streamingcommunity.ooo" // Images
+    override var mainUrl = "https://streamingcommunity.paris"
+    private var cdnUrl = "https://cdn.streamingcommunity.paris" // Images
     private var xInertiaVersion = "759de90f13813ff6f03369b34b51a141"
     override var name = "StreamingCommunity"
     override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
