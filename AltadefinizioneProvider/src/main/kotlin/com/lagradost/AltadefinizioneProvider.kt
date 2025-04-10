484f38d25c5059104668cfd5413de6a7e47738df
Antony Caporossi <antony.caporossi@gmail.com>
2025-04-10 23:09:09 +0200

    Altadefinizione completamente riscritto

--- AltadefinizioneProvider/src/main/kotlin/com/lagradost/AltadefinizioneProvider.kt
+++ AltadefinizioneProvider/src/main/kotlin/com/lagradost/AltadefinizioneProvider.kt
@@ -6,13 +6,16 @@ import com.lagradost.cloudstream3.LoadResponse.Companion.addRating
 import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.loadExtractor
+import com.lagradost.cloudstream3.utils.AppUtils.parseJson
+import com.lagradost.cloudstream3.utils.AppUtils.toJson
 import okhttp3.FormBody
+import android.util.Log
 import org.jsoup.nodes.Element
 
 
 class AltadefinizioneProvider : MainAPI() {
     override var lang = "it"
-    override var mainUrl = "https://altadefinizionegratis.cyou/"
+    override var mainUrl = "https://altadefinizione.gent/"
     override var name = "Altadefinizione"
     override val hasMainPage = true
     override val hasChromecastSupport = true
@@ -20,29 +23,67 @@ class AltadefinizioneProvider : MainAPI() {
         TvType.Movie
     )
 
-    override val mainPage = mainPageOf(
-        Pair("$mainUrl/cerca/anno/2024/page/", "Ultimi Film"),
-        Pair("$mainUrl/cerca/openload-quality/HD/page/", "Film in HD"),
-        Pair("$mainUrl/cinema/page/", "Ora al cinema")
-    )
-
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val url = request.data + page
-        val soup = app.get(url).document
-        val home = soup.select("div.box").mapNotNull {
-            it.toSearchResult()
+
+        val post = listOf(
+                mapOf("name" to "Cinema", "url" to "$mainUrl/cinema/"),
+                mapOf("name" to "Serie TV", "url" to "$mainUrl/serie-tv/"),
+                mapOf("name" to "Film", "url" to "$mainUrl/film/"),
+                mapOf("name" to "Azione", "url" to "$mainUrl/azione/"),
+                mapOf("name" to "Animazione", "url" to "$mainUrl/animazione/"),
+                mapOf("name" to "Avventura", "url" to "$mainUrl/avventura/"),
+                mapOf("name" to "Biografico", "url" to "$mainUrl/biografico/"),
+                mapOf("name" to "Commedia", "url" to "$mainUrl/commedia/"),
+                mapOf("name" to "Crime", "url" to "$mainUrl/crime/"),
+                mapOf("name" to "Documentario", "url" to "$mainUrl/documentario/"),
+                mapOf("name" to "Drammatico", "url" to "$mainUrl/drammatico/"),
+                mapOf("name" to "Famiglia", "url" to "$mainUrl/famiglia/"),
+                mapOf("name" to "Fantascienza", "url" to "$mainUrl/fantascienza/"),
+                mapOf("name" to "Fantasy", "url" to "$mainUrl/fantasy/"),
+                mapOf("name" to "Intrattenimento", "url" to "$mainUrl/intrattenimento/"),
+                mapOf("name" to "Giallo", "url" to "$mainUrl/giallo/"),
+                mapOf("name" to "Guerra", "url" to "$mainUrl/guerra/"),
+                mapOf("name" to "Horror", "url" to "$mainUrl/horror/"),
+                mapOf("name" to "Poliziesco", "url" to "$mainUrl/poliziesco/"),
+                mapOf("name" to "Romantico", "url" to "$mainUrl/romantico/"),
+                mapOf("name" to "Sitcom", "url" to "$mainUrl/sitcom/"),
+                mapOf("name" to "Soap opera", "url" to "$mainUrl/soap-opera/"),
+                mapOf("name" to "Spionaggio", "url" to "$mainUrl/spionaggio/"),
+                mapOf("name" to "Sentimentale", "url" to "$mainUrl/sentimentale/"),
+                mapOf("name" to "Sportivo", "url" to "$mainUrl/sportivo/"),
+                mapOf("name" to "Thriller", "url" to "$mainUrl/thriller/"),
+                mapOf("name" to "Western", "url" to "$mainUrl/western/")
+        )
+
+            val items: List<HomePageList> = post.map { postData ->
+                val soup = app.get(postData["url"]!!).document
+                val home = soup.select(".movie").mapNotNull { item ->
+                    item.toSearchResult()
+                }
+                HomePageList(postData["name"]!!, home)
+
+            }
+            if (items.isEmpty()) throw ErrorLoadingException()
+            return HomePageResponse(items, hasNext = true)
+        /*val soup = app.get(mainUrl).document
+        val items: List<HomePageList> = soup.select("main section:not(.slider)").mapNotNull { section ->
+            val name = section.selectFirst(".section-head h4")?.text()
+            val home = section.select(".swiper-slide").mapNotNull {
+                it.toSearchResult()
+            }
+            HomePageList(name ?: "", home)
         }
-        return newHomePageResponse(arrayListOf(HomePageList(request.name, home)), hasNext = true)
+        return HomePageResponse(items, hasNext = false)*/
     }
 
-    private fun Element.toSearchResult(): SearchResponse? {
-        val title = this.selectFirst("img")?.attr("alt") ?: return null
+    private fun Element.toSearchResult(isSwiper: Boolean = true): SearchResponse? {
+
+        val title = this.selectFirst("h2")?.text() ?: return null
         val link = this.selectFirst("a")?.attr("href") ?: return null
-        val image = fixUrl(this.selectFirst("img")?.attr("src") ?: "")
-        val quality = getQualityFromString(this.selectFirst("span")?.text())
-        return newMovieSearchResponse(title, link, TvType.Movie) {
+        val image = fixUrl(this.selectFirst("img")?.attr("data-src") ?: "")
+        val type = if (link.contains("serie-tv"))  TvType.TvSeries else TvType.Movie
+        return newMovieSearchResponse(title, link, type) {
             this.posterUrl = image
-            this.quality = quality
         }
     }
 
@@ -51,60 +92,98 @@ class AltadefinizioneProvider : MainAPI() {
             .addEncoded("do", "search")
             .addEncoded("subaction", "search")
             .addEncoded("story", query)
-            .addEncoded("sortby", "news_read")
             .build()
             
-        val doc = app.post(
-            "$mainUrl/index.php",
+        val doc = app.post(mainUrl,
             requestBody = body
         ).document
 
-        return doc.select("div.box").mapNotNull {
-            it.toSearchResult()
+        return doc.select("#dle-content .movie").mapNotNull {
+            it.toSearchResult(isSwiper = false)
         }
     }
 
     override suspend fun load(url: String): LoadResponse {
         val document = app.get(url).document
 
-        val title = document.selectFirst(" h1 > a")?.text()?.replace("streaming", "")
+        val title = document.selectFirst(" h1")?.text()?.replace("streaming", "")
             ?: throw ErrorLoadingException("No Title found")
-        val description = document.select("#sfull").textNodes().first { it.text().trim().isNotEmpty() }.text().trim()
-        val rating = document.select("span.rateIMDB").text().substringAfter(" ")
-        val year = document.selectFirst("#details")?.select("li")
-            ?.firstOrNull { it.select("label").text().contains("Anno") }
-            ?.text()?.substringAfter(" ")?.toIntOrNull()
-        val poster = fixUrl(document.selectFirst("div.thumbphoto > img")?.attr("src")?: throw ErrorLoadingException("No Poster found") )
-        val recomm = document.select("ul.related-list > li").mapNotNull {
-            it.toSearchResult()
-        }
-        val actors: List<Actor> =
-            document.select("#staring > a").map {
-                Actor(it.text())
-            }
-        val tags: List<String> = document.select("#details > li:nth-child(1) > a").map { it.text() }
-        val trailerUrl = document.selectFirst("#showtrailer > div > div > iframe")?.attr("src")
+        val container = document.selectFirst(".text-container")
+        container?.select(".dots, .more-link")?.remove() // rimuove i pezzi inutili
+        val description = container?.text()
+        val rating = document.select("span.imdb").text()
+        val poster = fixUrl(document.selectFirst("img.movie_entry-poster")?.attr("data-src")?: throw ErrorLoadingException("No Poster found") )
+
+        val year = document.select("div.movie_entry-details > div:nth-child(1) > div:nth-child(2)").text().toIntOrNull()
+        val tags: List<String> = document.select(".movie_entry-details > div:nth-child(2) a").map { it.text() }
+        val trailerUrl = document.selectFirst("iframe#trailer-embed")?.attr("src")
         val iframeSrc = document
-        .select("iframe:not([id=mirrorFrame])")
+        .select(".guardahd-player iframe")
         .first() // Get the first matching iframe
-        ?.attr("src")
+        ?.attr("data-src")
+        
+        val type = if (url.contains("serie-tv"))  TvType.TvSeries else TvType.Movie
+
+        if (type == TvType.TvSeries) {
+            val episodeList = ArrayList<Episode>()
+
+
+            document.select(".dropdown.mirrors").forEach { episode ->
+                val season = episode.attr("data-season").toInt()
+                val epNum = episode.attr("data-episode").substringAfter("-").toInt()
+                val title = "Episodio $epNum"
+                val hrefs = episode.select(".mirrors").select(".dropdown-item").map { it.attr("data-link") }
+
+                val data = LoadLinkData(
+                    links = hrefs,
+                    type = TvType.TvSeries
+                ).toJson()
+                episodeList.add(
+                    Episode(
+                        name = "Episodio $epNum",
+                        data = data,
+                        season = season,
+                        episode = epNum,
+                    )
+                )
+            }
+            return newTvSeriesLoadResponse(
+                title,
+                url,
+                TvType.TvSeries,
+                episodeList
+            ) {
+                addPoster(poster)
+                this.year = year
+                this.plot = description
+                addRating(rating)
+                this.tags = tags
+                addTrailer(trailerUrl)
+            }
+        }
+
+        val data = LoadLinkData(
+            links = listOf(iframeSrc!!),
+            type = TvType.Movie
+        ).toJson()
+
         return newMovieLoadResponse(
             title,
             url,
             TvType.Movie,
-            iframeSrc
+            data
         ) {
             this.year = year
             this.plot = description
-            this.recommendations = recomm
             this.tags = tags
-            addActors(actors)
             addPoster(poster)
             addRating(rating)
             addTrailer(trailerUrl)
         }
     }
-
+    private data class LoadLinkData(
+        val type: TvType = TvType.Movie, val links: List<String>? = null
+    )
 
     override suspend fun loadLinks(
         data: String,
@@ -112,22 +191,22 @@ class AltadefinizioneProvider : MainAPI() {
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ): Boolean {
-        val doc = app.get(data).document
-        loadExtractor("prova", data, subtitleCallback, callback)
-        /*if (doc.select("div.guardahd-player").isNullOrEmpty()) {
-            val videoUrl =
-                doc.select("input").last { it.hasAttr("data-mirror") }.attr("value")
-            loadExtractor(videoUrl, data, subtitleCallback, callback)
-            doc.select("#mirrors > li > a").forEach {
-                loadExtractor(fixUrl(it.attr("data-target")), data, subtitleCallback, callback)
-            }
+        var links = ArrayList<String>()
+        val dataJson = parseJson<LoadLinkData>(data)
+        if (dataJson.type == TvType.TvSeries) {
+            links.addAll(dataJson.links!!)
         } else {
-            val pagelinks = doc.select("div.guardahd-player").select("iframe").attr("src")
-            val docLinks = app.get(pagelinks).document
-            docLinks.select("body > div > ul > li").forEach {
-                loadExtractor(fixUrl(it.attr("data-link")), data, subtitleCallback, callback)
+            val _url = dataJson.links?.joinToString("") ?: ""
+            val doc = app.get(_url).document
+            doc.select("._player-mirrors li").forEach{
+                links.add(
+                    fixUrl(it.attr("data-link"))
+                )
             }
-        }*/
+        }
+        links.forEach { link ->
+            loadExtractor(link, data, subtitleCallback, callback)
+        }
         return true
     }
 }
